4a5fbd4ffca8470c19282d9a2d6930abbb8f67d7
Matsushoooo12 <matsush.oooo12@icloud.com>
2022-06-11 20:06:02 +0900

    コンポーネント修正

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import Login from "./components/auth/Login";
 import Home from "./components/Home";
 
 const App: React.FC = () => {
   return (
     <>
       <Home />
+      <Login />
     </>
   );
 };
